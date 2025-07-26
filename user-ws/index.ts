856c022f7ec4c76e35f9d3536a715b89c986bfba
kunal.palwar <kunal.palwar@businessnext.com>
2025-07-26 23:59:26 +0530

    init boiler-plate for user-chat

---
+++ user-ws/index.ts
@@ -0,0 +1,17 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+interface Rooms {
+    sockets: WebSocket[];
+}
+const rooms: Record<string, WebSocket> = {};
+
+wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+
+    ws.send('something');
+});
